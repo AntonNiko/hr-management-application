b6c43b3cf0106f9e35cb8185ece6793a7a1e3812
AntonNiko <ant.nikitenko@gmail.com>
2018-08-09 14:46:26 -0700

    Added initial ASP.NET project template from VS2017

---
+++ hr-management-application/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace hr_management_application.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ hr-management-application/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hr_management_application.Startup))]
+namespace hr_management_application
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
