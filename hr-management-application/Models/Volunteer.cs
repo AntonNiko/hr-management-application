2f8c85acc32c73e833ab9a2024a57098c8c40c48
AntonNiko <ant.nikitenko@gmail.com>
2018-08-16 01:11:07 -0700

    Added Controller templates for models, and corresponding index views for each controller

--- hr-management-application/Models/Volunteer.cs
+++ hr-management-application/Models/Volunteer.cs
@@ -7,5 +7,25 @@ namespace hr_management_application.Models
 {
     public class Volunteer
     {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public string StreetName { get; set; }
+        public string HouseNum { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+
+        public string PhoneNum { get; set; }
+        public string Email { get; set; }
+
+        public string Position { get; set; }
+        public Wage EmployeeWage { get; set; }
+        public Salary EmployeeSalary { get; set; }
+        public Payroll PayrollInfo { get; set; }
+
+        public string EmergContactName { get; set; }
+        public string EmergContactPhone { get; set; }
     }
 }
